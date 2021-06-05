d0b808c9da0dad37104b68fd8d384aeb02400b5b
hmney <houssamemney98@gmail.com>
2021-06-05 09:36:50 +0100

    add app module

--- lib/core/app/app_module.dart
+++ lib/core/app/app_module.dart
@@ -1,3 +1,17 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mvp_assessement/core/auth/auth_module.dart';
+import 'package:mvp_assessement/core/auth/controllers/auth_controller.dart';
+import 'package:mvp_assessement/core/auth/screens/auth_page.dart';
 
-class AppModule extends Module {}
+class AppModule extends Module {
+  @override
+  final List<Bind> binds = [
+    Bind((i) => AuthController()),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ModuleRoute('/auth', module: AuthModule()),
+    ChildRoute(Modular.initialRoute, child: (_, args) => AuthPage())
+  ];
+}
--- lib/core/app/app_widget.dart
+++ lib/core/app/app_widget.dart
@@ -1,14 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
 
+  @override
+  _AppWidgetState createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return MaterialApp(
+              home: Scaffold(body: Text('Error while loading fireflutter')));
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
     ).modular();
   }
 }
