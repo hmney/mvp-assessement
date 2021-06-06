6f8dd7d87de9261d63df99ca460cf238ef2fa706
hmney <houssamemney98@gmail.com>
2021-06-06 09:39:04 +0100

    add custom elevated button widget

---
+++ lib/widgets/custom_elevated_button_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomElevatedButtonWidget extends StatelessWidget {
+  const CustomElevatedButtonWidget({
+    Key? key,
+    required this.onPressed,
+    required this.label,
+    required this.icon,
+    this.width,
+  }) : super(key: key);
+
+  final VoidCallback onPressed;
+  final String label;
+  final IconData icon;
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(flex: 2),
+            Text(label),
+            Spacer(flex: 2),
+            Icon(icon),
+          ],
+        ),
+      ),
+    );
+  }
+}
