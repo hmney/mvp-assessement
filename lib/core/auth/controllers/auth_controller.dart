e85b06caa5dce6fd5bc6c12d1fa94ead2200e283
hmney <houssamemney98@gmail.com>
2021-06-06 09:40:36 +0100

    add auth controller

--- lib/core/auth/controllers/auth_controller.dart
+++ lib/core/auth/controllers/auth_controller.dart
@@ -1,7 +1,11 @@
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
+import 'package:mvp_assessement/core/auth/repositories/auth_repository.dart';
 
 part 'auth_controller.g.dart';
 
 class AuthController = _AuthController with _$AuthController;
 
-abstract class _AuthController with Store {}
+abstract class _AuthController with Store {
+  final authRepository = Modular.get<AuthRepository>();
+}
