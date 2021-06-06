7c4e927eaeecea52c8fa8bbcf265a1e58937ff57
hmney <houssamemney98@gmail.com>
2021-06-06 16:14:45 +0100

    remove unnecessary imports

--- lib/core/app/app_module.dart
+++ lib/core/app/app_module.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mvp_assessement/core/auth/auth_module.dart';
-import 'package:mvp_assessement/core/auth/controllers/auth_controller.dart';
-import 'package:mvp_assessement/core/auth/repositories/auth_repository.dart';
-import 'package:mvp_assessement/core/auth/screens/auth_page.dart';
 
 class AppModule extends Module {
   @override
