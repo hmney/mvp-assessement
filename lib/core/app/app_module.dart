8f0847f045a461b52e86d703fae159802e171066
hmney <houssamemney98@gmail.com>
2021-06-06 09:39:39 +0100

    refactor the auth module

--- lib/core/app/app_module.dart
+++ lib/core/app/app_module.dart
@@ -1,17 +1,12 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mvp_assessement/core/auth/auth_module.dart';
 import 'package:mvp_assessement/core/auth/controllers/auth_controller.dart';
+import 'package:mvp_assessement/core/auth/repositories/auth_repository.dart';
 import 'package:mvp_assessement/core/auth/screens/auth_page.dart';
 
 class AppModule extends Module {
   @override
-  final List<Bind> binds = [
-    Bind((i) => AuthController()),
-  ];
-
-  @override
   final List<ModularRoute> routes = [
-    ModuleRoute('/auth', module: AuthModule()),
-    ChildRoute(Modular.initialRoute, child: (_, args) => AuthPage())
+    ModuleRoute('', module: AuthModule()),
   ];
 }
