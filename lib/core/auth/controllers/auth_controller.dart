4ac7fe1ea37c6b5de3560e6466022723aca13ebb
hmney <houssamemney98@gmail.com>
2021-06-06 16:15:12 +0100

    add auth controller

--- lib/core/auth/controllers/auth_controller.dart
+++ lib/core/auth/controllers/auth_controller.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:mvp_assessement/core/auth/repositories/auth_repository.dart';
@@ -8,4 +10,11 @@ class AuthController = _AuthController with _$AuthController;
 
 abstract class _AuthController with Store {
   final authRepository = Modular.get<AuthRepository>();
+  final Future<FirebaseApp> initFirebaseApp = Firebase.initializeApp();
+  Stream<User?> authStateChangesStream() =>
+      authRepository.firebaseAuth.authStateChanges();
+
+  void signInWithGoogle() => authRepository.signInWithGoogle();
+  void signInWithFacebook() => authRepository.signInWithFacebook();
+  void signOut() => authRepository.signOut();
 }
