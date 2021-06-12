f4cfaf5c5f8095f5f9469a77859235cc7e720754
hmney <houssamemney98@gmail.com>
2021-06-12 18:21:03 +0100

    refactor the main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mvp_assessement/core/app/app_module.dart';
@@ -7,8 +6,6 @@ import 'core/app/app_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
   runApp(
     ModularApp(module: AppModule(), child: AppWidget()),
   );
