0691ef59402379f966182e53f18eee8f0e379870
hmney <houssamemney98@gmail.com>
2021-06-12 18:13:14 +0100

    remove unnecessary import

--- lib/core/app/app_widget.dart
+++ lib/core/app/app_widget.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mvp_assessement/config/themes/light_theme.dart';
