0d2bdebbca4789ce877c5e72a097c40580909b53
hmney <houssamemney98@gmail.com>
2021-06-12 18:21:21 +0100

    create app module

--- lib/core/app/app_module.dart
+++ lib/core/app/app_module.dart
@@ -1,9 +1,22 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mvp_assessement/core/app/pages/splash_page.dart';
 import 'package:mvp_assessement/core/auth/auth_module.dart';
+import 'package:mvp_assessement/modules/projects/projects_module.dart';
 
 class AppModule extends Module {
   @override
   final List<ModularRoute> routes = [
-    ModuleRoute('', module: AuthModule()),
+    ChildRoute(
+      Modular.initialRoute,
+      child: (context, args) => SplashPage(),
+    ),
+    ModuleRoute(
+      '',
+      module: AuthModule(),
+    ),
+    ModuleRoute(
+      '',
+      module: ProjectsModule(),
+    ),
   ];
 }
